50b8828e0712d2f5364a1eb04111b132add1fbea
Afiv Dicky Efendy <dickyareda@gmail.com>
2023-06-07 22:50:29 +0700

    enable cors

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+
+// enable cors
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
